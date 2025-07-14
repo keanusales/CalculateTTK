483ebe6bebc9b1d1c08c609b804e4bf165306f5b
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2025-07-14 11:16:19 -0300

    Update calculatettk.py

--- calculatettk.py
+++ calculatettk.py
@@ -4,39 +4,35 @@ from tkinter import Text, Tk
 from re import fullmatch
 from math import ceil
 
-############################
-###    TTK CALCULATOR    ###
-############################
-
 bodyparts = ("Head", "Chest", "Belly", "Arms", "Forearms", "Thighs", "Legs")
 
-def get_ttk(damages: list[float], drops: list[float], rate: float) -> list[list[float]]:
+get_ttk_rtype = tuple[list[list[float]], float]
+def get_ttk(damages: list[float], drops: list[float], rate: float) -> get_ttk_rtype:
   if (len(damages) != len(bodyparts) or not all(d > 0 for d in damages)
       or not drops or not all(0 < d <= 1 for d in drops) or not (rate > 0)):
     raise ValueError("There is an invalid input. Ensure the values are correct.")
-  return [[(60000 / rate) * (ceil(100 / d / n) - 1) for n in drops] for d in damages]
+  punishment = 60000 / rate
+  ttks = [[punishment * (ceil(100 / d / n) - 1) for n in drops] for d in damages]
+  return ttks, punishment
 
-############################
-###   TABLE GENERATION   ###
-############################
-
-def get_table(damages: list[float], drops: list[float], rate: float) -> tuple[str, int]:
+def table(damages: list[float], drops: list[float], rate: float) -> tuple[str, int]:
   hor, ver, tleft, tright, bleft, bright = "═", "║", "╔", "╗", "╚", "╝"
   tjoin, bjoin, ljoin, rjoin, mjoin = "╦", "╩", "╠", "╣", "╬"
 
-  rows = ([["Part/Drop"] + [f"{drop}x" for drop in drops]]
-    + [[part] + [f"{ttk:.1f}" for ttk in ttks] for part, ttks
-      in zip(bodyparts, get_ttk(damages, drops, rate))])
+  ttks, punishment = get_ttk(damages, drops, rate)
+  rows = ([["Part/Drop"] + [f"{drop}x" for drop in drops]] +
+    [[p] + [f"{v:.1f}" for v in ts] for p, ts in zip(bodyparts, ttks)])
   widths = [max(len(value) for value in column) for column in zip(*rows)]
 
   def line(left: str, join: str, right: str) -> str:
     return left + join.join(hor * (w + 2) for w in widths) + right
 
   max_width = sum(widths) + 3 * len(widths) + 1
+  title = f" TTK Calculator {hor} Punishment: {punishment:.1f} ms "
   middle_line, len_rows = line(ljoin, mjoin, rjoin), (len(rows) - 1)
 
   monted_table = "\n".join([line(tleft, hor, tright)]
-    + [ljoin + " Miliseconds TTKs ".center(max_width - 2, hor) + rjoin]
+    + [ljoin + title.center(max_width - 2, hor) + rjoin]
     + [line(ljoin, tjoin, rjoin)]
     + [item for i, row in enumerate(rows) for item in
       ["".join(f"{ver} {c.ljust(w)} " for c, w in zip(row, widths)) + ver]
@@ -44,10 +40,6 @@ def get_table(damages: list[float], drops: list[float], rate: float) -> tuple[st
     + [line(bleft, bjoin, bright)])
 
   return monted_table, max_width
-
-############################
-###    MAIN INTERFACE    ###
-############################
 
 def main_interface(root: Tk) -> None:
   root.title("Delta Force TTK Calculator")
@@ -98,7 +90,7 @@ def main_interface(root: Tk) -> None:
       damages = [parse_damage(e.get()) for e in damages_entry]
       drops = (float(e) for e in drop_entry.get().split())
       drops = sorted(drops, reverse = True)
-      show_results(*get_table(damages, drops, rate))
+      show_results(*table(damages, drops, rate))
     except Exception as e:
       show_results(f"An internal error occurred: {e!s}", 40)
 
@@ -132,9 +124,5 @@ def main_interface(root: Tk) -> None:
   for child in frame1.winfo_children(): child.grid(padx = 5, pady = 5)
 
   root.mainloop()
-
-#############################
-###    RUNNING THE APP    ###
-#############################
 
 if __name__ == "__main__": main_interface(root = Tk())
