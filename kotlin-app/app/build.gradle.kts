3a92e441a994f14e13c7ce3af2be48c196627879
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2026-05-09 23:05:41 -0300

    Update Compose BOM version

--- kotlin-app/app/build.gradle.kts
+++ kotlin-app/app/build.gradle.kts
@@ -80,7 +80,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.18.0")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation(platform("androidx.compose:compose-bom:2026.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2026.05.00"))
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
