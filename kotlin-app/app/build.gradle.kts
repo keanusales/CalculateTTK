740c00132963453904c9c0e1514a6178ade4bad0
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2026-07-08 23:53:10 -0300

    Update build.gradle.kts

--- kotlin-app/app/build.gradle.kts
+++ kotlin-app/app/build.gradle.kts
@@ -81,7 +81,7 @@ android {
 dependencies {
   implementation("androidx.core:core-ktx:1.19.0")
   implementation("androidx.activity:activity-compose:1.13.0")
-  implementation(platform("androidx.compose:compose-bom:2026.06.00"))
+  implementation(platform("androidx.compose:compose-bom:2026.06.01"))
   implementation("androidx.compose.material:material-icons-core")
   implementation("androidx.compose.material3:material3")
   implementation("androidx.compose.ui:ui")
