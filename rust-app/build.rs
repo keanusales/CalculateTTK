be26673bee25d07b0427cac394d63287f3b546fc
keanusales <100793959+keanusales@users.noreply.github.com>
2026-07-20 11:15:52 -0300

    Update build.rs

--- rust-app/build.rs
+++ rust-app/build.rs
@@ -1,7 +1,8 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-  if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+  #[cfg(windows)]
+  {
     let mut res = winres::WindowsResource::new();
     res.set_icon("icon.ico"); 
     res.compile()?;
